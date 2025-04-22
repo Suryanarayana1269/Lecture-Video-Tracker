e99961a1f519ddeb70ff6f11c677fe56ee51b837
Suryanarayana Bodapati <166002441+Suryanarayana1269@users.noreply.github.com>
2025-04-23 01:12:55 +0530

    Update authService.jsx

--- client/src/services/authService.jsx
+++ client/src/services/authService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://lecture-video-tracker.onrender.com/api/auth";
 
 // Login user and store token in localStorage
 export const login = async (email, password) => {
